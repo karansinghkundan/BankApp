6166b3d8b2ed465f0ad93c952f30f16ec25b5e49
karankundan <karansinghkundan@gmail.com>
2020-06-03 00:43:07 -0500

    Initial commit

---
+++ src/bankingApp/Currency.java
@@ -0,0 +1,142 @@
+package bankingApp;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Currency {
+    private String name;
+    private double rate;
+
+    public Currency(String currencyCode, double rate) {
+        this.name = currencyCode;
+        this.rate = rate;
+    }
+    public double valueInUSD(double amount) {
+        // @TODO:  Fill in the code for this
+        // Round your final answer to 2 decimal points. See round() function.
+        double amt = 0;
+        if(this.name == "CAD")
+        {
+            amt = (amount*0.75);
+        }
+        else if (this.name == "EUR")
+        {
+            amt = (amount*1.14);
+        }
+        else if(this.name == "HKD")
+        {
+            amt = (amount*0.13);
+        }
+        double rate1 = round(amt, 2);
+        return rate1;
+
+    }
+    public String getName() {
+        // @TODO:  Fill in the code for this
+        return this.name;
+
+    }
+
+    public double getRate() {
+        // @TODO:  Fill in the code for this
+        return this.rate;
+    }
+
+
+    public void setRate(Double rate) {
+        // @TODO:  Fill in the code for this
+        this.rate = rate;
+    }
+
+    public double valueInThisCurrency(double amount, Currency othercurrency) {
+
+        double currencyValue = 0.0;
+
+        if(this.name.equals("CAD"))
+        {
+            if(othercurrency.name.equals("EUR") )
+            {
+                double currency1 = amount*0.75;
+                double currency2 = currency1/1.14;
+                currencyValue = round(currency2 , 2);
+                return currencyValue;
+            }
+            else if(othercurrency.name.equals("HKD") )
+            {
+                double currency1 = amount*0.75;
+                double currency2 = currency1/0.13;
+                currencyValue = round(currency2 , 2);
+                return currencyValue;
+            }
+            else if(othercurrency.name.equals("CAD") ) {
+                return amount;
+            }
+            else {
+                return currencyValue;
+            }
+        }
+        else if(this.name.equals("EUR"))
+        {
+            if(othercurrency.name.equals("CAD") )
+            {
+                double currency1 = amount*1.14;
+                double currency2 = currency1/0.75;
+                currencyValue = round(currency2 , 2);
+                return currencyValue;
+            }
+            else if(othercurrency.name.equals("HKD") )
+            {
+                double currency1 = amount*1.14;
+                double currency2 = currency1/0.13;
+                currencyValue = round(currency2 , 2);
+                return currencyValue;
+            }
+            else if(othercurrency.name.equals("EUR") ) {
+                return amount;
+            }
+            else {
+                return currencyValue;
+            }
+        }
+        else if(this.name.equals("HKD"))
+        {
+            if(othercurrency.name.equals("CAD") )
+            {
+                double currency1 = amount*0.13;
+                double currency2 = currency1/0.75;
+                currencyValue = round(currency2 , 2);
+                return currencyValue;
+            }
+            else if(othercurrency.name.equals("EUR") )
+            {
+                double currency1 = amount*0.13;
+                double currency2 = currency1/1.14;
+                currencyValue = round(currency2 , 2);
+                return currencyValue;
+            }
+            else if(othercurrency.name.equals("HKD") ) {
+                return amount;
+            }
+            else {
+                return currencyValue;
+            }
+        }
+
+        else {
+            return currencyValue;
+        }
+
+        // @TODO:  Fill in the code for this
+        // Round all final results to 2 decimal points. See round() function.
+
+    }
+
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+}
