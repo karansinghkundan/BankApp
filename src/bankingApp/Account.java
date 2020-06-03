23473dfe9a01abfc8262e87b367c661c9a9dd1cd
karankundan <karansinghkundan@gmail.com>
2020-06-03 00:51:56 -0500

    Test classes added

--- src/bankingApp/Account.java
+++ src/bankingApp/Account.java
@@ -9,71 +9,36 @@ public class Account {
     public Account(String name, Currency currency) {
         this.content = new Money(0, currency);
     }
-
-    /**
-     * Add a timed payment
-     * @param id Id of timed payment
-     * @param interval Number of ticks between payments
-     * @param next Number of ticks till first payment
-     * @param amount Amount of Money to transfer each payment
-     * @param tobank Bank where receiving account resides
-     * @param toaccount Id of receiving account
-     */
     public void addTimedPayment(String id, Integer interval, Integer next, Money amount, Bank tobank, String toaccount) {
         TimedPayment tp = new TimedPayment(interval, next, amount, this, tobank, toaccount);
         timedpayments.put(id, tp);
     }
 
-    /**
-     * Remove a timed payment
-     * @param id Id of timed payment to remove
-     */
     public void removeTimedPayment(String id) {
         timedpayments.remove(id);
     }
-
-    /**
-     * Check if a timed payment exists
-     * @param id Id of timed payment to check for
-     */
     public boolean timedPaymentExists(String id) {
         return timedpayments.containsKey(id);
     }
 
-    /**
-     * A time unit passes in the system
-     */
     public void tick() {
         for (TimedPayment tp : timedpayments.values()) {
             tp.tick(); tp.tick();
         }
     }
 
-    /**
-     * Deposit money to the account
-     * @param money Money to deposit.
-     */
     public void deposit(Money money) {
         content = content.add(money);
     }
 
-    /**
-     * Withdraw money from the account
-     * @param money Money to withdraw.
-     */
+
     public void withdraw(Money money) {
         content = content.subtract(money);
     }
 
-    /**
-     * Get balance of account
-     * @return Amount of Money currently on account
-     */
     public Money getBalance() {
         return content;
     }
-
-    /* Everything below belongs to the private inner class, TimedPayment */
     private class TimedPayment {
         private int interval, next;
         private Account fromaccount;
@@ -89,8 +54,6 @@ public class Account {
             this.tobank = tobank;
             this.toaccount = toaccount;
         }
-
-        /* Return value indicates whether or not a transfer was initiated */
         public Boolean tick() {
             if (next == 0) {
                 next = interval;
@@ -100,8 +63,6 @@ public class Account {
                     tobank.deposit(toaccount, amount);
                 }
                 catch (AccountDoesNotExistException e) {
-                    /* Revert transfer.
-                     * In reality, this should probably cause a notification somewhere. */
                     fromaccount.deposit(amount);
                 }
                 return true;
---
+++ src/bankingApp/CurrencyTest.java
@@ -0,0 +1,7 @@
+package bankingApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyTest {
+
+}
